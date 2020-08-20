cc714b6602f88fe056e0cc5867c87855fdc6d7f4
Celine Stalder <celine.stalder@student.unisg.ch>
2020-08-20 17:06:02 +0100

    Mr. Cocktail first versio

---
+++ db/migrate/20200820110416_add_column_to_dose.rb
@@ -0,0 +1,5 @@
+class AddColumnToDose < ActiveRecord::Migration[6.0]
+  def change
+    add_column :doses, :description, :string
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'json'
+require 'open-uri'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredient_serialized = open(url).read
+ingredient = JSON.parse(ingredient_serialized)
+
+10.times do
+  Ingredient.create(name: ingredient["drinks"].sample["strIngredient1"])
+end
+
+Cocktail.create(name: "First cocktail")
